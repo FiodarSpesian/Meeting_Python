45cce2891f35538c1968e83c7afdeb70a9910184
Fiodar <fiodarspesian@mail.ru>
2023-01-29 14:49:57 +0300

    add decision case3

--- Lesson1/Case3.py
+++ Lesson1/Case3.py
@@ -6,3 +6,13 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+st_n = input("Enter your positive number n: ")
+n = int(st_n)
+st_res = ""
+i = 0
+res = 0
+while i < n:
+    st_res = st_res + st_n
+    res = res + int(st_res)
+    i += 1
+print(f'Sum of your numbers of n: {res}')
