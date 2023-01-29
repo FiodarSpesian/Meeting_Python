45886804eb815a2945f323f5a6a427ca010e5da3
Fiodar <fiodarspesian@mail.ru>
2023-01-29 16:08:24 +0300

    add decision of case1 l2

--- Lesson2/case1.py
+++ Lesson2/case1.py
@@ -17,3 +17,13 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 """
+a = float(input('How many kilometers you run today: '))
+b = float(input('How many kilometers you want to run: '))
+counter = 1
+while a < b:
+    print(f'{counter} day: {round(a, 2)}')
+    a = a + a / 10
+    counter += 1
+
+print(f'{counter} day: {round(a, 2)}')
+print(f'Answer: on {counter} day sportsmen reach the goal - more than {round(a, 2)} km')
