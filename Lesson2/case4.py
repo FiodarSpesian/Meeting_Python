a57e2c1e8560919a044c93cb160b01ad61e088c8
Fiodar <fiodarspesian@mail.ru>
2023-02-01 23:00:52 +0300

    add decision of case4

--- Lesson2/case4.py
+++ Lesson2/case4.py
@@ -10,3 +10,21 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+lst = input('Enter values by the space: ').split()
+print(f'Your list: {lst}')
+i = 0
+ln = len(lst)
+if ln % 2 == 0:
+    while i < ln:
+        temp = lst[i]
+        lst[i] = lst[i + 1]
+        lst[i + 1] = temp
+        i += 2
+else:
+    while i < ln - 1:
+        temp = lst[i]
+        lst[i] = lst[i + 1]
+        lst[i + 1] = temp
+        i += 2
+
+print(f'Result: {lst}')
