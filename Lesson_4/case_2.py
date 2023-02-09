c973fa13538619d69f4b6ff46d10d7eb22a82b58
Fiodar <fiodarspesian@mail.ru>
2023-02-09 15:44:50 +0300

    add decision of case 2

--- Lesson_4/case_2.py
+++ Lesson_4/case_2.py
@@ -2,3 +2,12 @@
 Задайте натуральное число N.
 Напишите программу, которая составит список простых множителей числа N.
 """
+num = int(input(f"Enter number: "))
+lst = []
+for i in range(num):
+    if num % (i + 1) == 0:
+        lst.append(i+1)
+    for el in lst:
+        if el % 2 == 0:
+            lst.remove(el)
+print(lst)
