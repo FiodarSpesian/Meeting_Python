81b7a33c3c34ab9819738e3c2ef5d9aad2224df4
Fiodar <fiodarspesian@mail.ru>
2023-02-17 21:15:58 +0300

    add decision of case 1 seminar 6

--- Lesson_6/case_1.py
+++ Lesson_6/case_1.py
@@ -4,3 +4,21 @@
 Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 Каждое число вводится с новой строки.
 """
+a = int(input("первый элемент: "))
+d = int(input("разность: "))
+n = int(input("количество элементов: "))
+array = list()
+
+
+def math_progression(f_el, diff, count, lst):
+    if count == 0:
+        return lst.reverse()
+    res = f_el + (count - 1) * diff
+    lst.append(res)
+    print(f"{count} element is {res}")
+    count -= 1
+    math_progression(f_el, diff, count, lst)
+
+
+math_progression(a, d, n, array)
+print(array)
