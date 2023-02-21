ca7435790b479c53d1623d3fd5ff703d0d7ee1e8
Fiodar <fiodarspesian@mail.ru>
2023-02-22 00:54:37 +0300

    Add decision of case 3 seminar 7

--- Lesson_7/case_3.py
+++ Lesson_7/case_3.py
@@ -11,3 +11,36 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку str
 str(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    count = 0
+
+    def __init__(self):
+        Worker.count += 1
+        self.name = "Name"
+        self.surname = "Surname"
+        self.position = "Position"
+        self._income = {}
+
+
+class Position(Worker):
+    def get_full_name(self, name, surname):
+        self.name = name
+        self.surname = surname
+        return
+
+    def get_total_income(self, wage, bonus):
+        self._income["wage"] = wage
+        self._income["bonus"] = bonus
+        return
+
+
+a = Position()
+a.get_full_name("Fiodor", "Spetsyian")
+a.get_total_income(20000, 5000)
+print(f"{Worker.count}) Employee {a.name} {a.surname} income: {a._income}")
+b = Position()
+b.get_full_name("Sergey", "Kudin")
+b.get_total_income(19000, 6000)
+print(f"{Worker.count}) Employee {b.name} {b.surname} income: {b._income}")
