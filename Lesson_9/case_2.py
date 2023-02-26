556c4d73651145435544e3dd40b53da1b1ab6c33
Fiodar <fiodarspesian@mail.ru>
2023-02-26 21:37:45 +0300

    add decision of case 2 seminar 9

--- Lesson_9/case_2.py
+++ Lesson_9/case_2.py
@@ -1,3 +1,26 @@
 """
 Создать метакласс для паттерна Синглтон (см. конец вебинара)
 """
+
+
+class MyMeto(type):
+    el = None
+
+    def __call__(self, *args, **kwargs):
+        if self.el == None:
+            return self.el
+
+
+class TestClass(metaclass=MyMeto):
+
+    def function(self):
+        pass
+
+
+a = TestClass()
+b = TestClass()
+c = TestClass()
+d = TestClass()
+print(a is b)
+print(a is d)
+print(c is d)
