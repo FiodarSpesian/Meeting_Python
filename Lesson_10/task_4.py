7e2c4ee4dd6921496abe4b580ac711d5ff2b7ff9
Fiodar <fiodarspesian@mail.ru>
2023-03-03 22:43:16 +0300

    add decision of case 4 seminar 10

--- Lesson_10/task_4.py
+++ Lesson_10/task_4.py
@@ -8,3 +8,13 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+lst = ["разработка", "администрирование", "protocol", "standard"]
+n_lst1 = []
+n_lst2 = []
+for el in lst:
+    n_lst1.append(el.encode("utf-8"))
+for el in n_lst1:
+    n_lst2.append(el.decode("utf-8"))
+print(lst)
+print(n_lst1)
+print(n_lst2)
