43c2c5ea48d7ab0440f9920152b48d76bb125dd2
Fiodar <fiodarspesian@mail.ru>
2023-03-03 23:36:53 +0300

    add decision of case 5 seminar 10

--- Lesson_10/task_5.py
+++ Lesson_10/task_5.py
@@ -7,3 +7,17 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+lst1 = ['ping', 'yandex.ru']
+YA_PING = subprocess.Popen(lst1, stdout=subprocess.PIPE)
+print(YA_PING.stdout)
+for line in YA_PING.stdout:
+    res1 = chardet.detect(line)
+    print(line.decode(res1['encoding']))
+lst2 = ['ping', 'youtube.com']
+YT_PING = subprocess.Popen(lst2, stdout=subprocess.PIPE)
+print(YT_PING.stdout)
+for line in YT_PING.stdout:
+    res2 = chardet.detect(line)
+    print(line.decode(res2['encoding']))
